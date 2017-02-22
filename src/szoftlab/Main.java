6967d4b1bbf43966269dcf855bdd244cb63aaaef
csukerdamarton <csukerdamarton@dhcp-2001.q.wlan.net.bme.hu>
2017-02-22 13:14:08 +0100

    x fv

--- src/szoftlab/Main.java
+++ src/szoftlab/Main.java
@@ -7,10 +7,14 @@ public class Main {
 		System.out.println("teszt2");
 		
 		System.out.println(add(3,4));
+		System.out.println(x(2,3));
 	}
 	
 	static int add(int a, int b){
 		return a+b;
+	}
+	static int x(int a, int b){
+		return a*b;
 	}
 
 }
