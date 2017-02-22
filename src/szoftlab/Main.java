66bc51189f574d7812e8bc2754a4d6d979afbc8f
csukerdamarton <csukerdamarton@dhcp-2001.q.wlan.net.bme.hu>
2017-02-22 13:31:40 +0100

    team projeccct

--- src/szoftlab/Main.java
+++ src/szoftlab/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("teszt");
 		System.out.println("teszt2");
-		System.out.println("Nagyon team project");
+		System.out.println("Nagyon team projeccct");
 		System.out.println(add(3,4));
 		System.out.println(x(2,3));
 	}
