5d2ccdd829b4f3f25dc80a6f3a86cd3f63aa16ee
Borbola Martin <borbolam14@gmail.com>
2017-02-22 12:59:46 +0100

    Creating add(a,b) function Testing add(a,b) function

--- src/szoftlab/Main.java
+++ src/szoftlab/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("teszt");
 		System.out.println("teszt2");
+		
+		System.out.println(add(3,4));
+	}
+	
+	static int add(int a, int b){
+		return a+b;
 	}
 
 }
