18f9e870f86d804361386c4e8cb4ffece46793ce
Máté <Mt@Blazestation-III>
2017-03-19 16:41:55 +0100

    Rail csiszolása

--- src/szoftlab/Rail.java
+++ src/szoftlab/Rail.java
@@ -4,6 +4,7 @@ public class Rail {		//A sín osztály, a többi pályaelem is ebbõl származik.
 	protected Rail neighbour_one;
 	protected Rail neighbour_two;
 	protected String name;
+	protected boolean usedby;
 	
 	public Rail whosNext(Rail lastplace){ //Ezzel kéri a mozdony, hogy melyik sínre kell mennie
 		if (lastplace==neighbour_one){
@@ -15,5 +16,17 @@ public class Rail {		//A sín osztály, a többi pályaelem is ebbõl származik.
 		return "Rail "+name;
 	}
 	
+	public boolean amIUsed(){ //Megkérdezzük, hogy használatban van-e
+		return usedby;
+	}
+	
+	public void setUsedBy(boolean used){ //Beállítjuk, hogy használva van-e a sín
+		this.usedby=used;
+	}
+	
+	public void connect(Rail one, Rail two){
+		this.neighbour_one=one;
+		this.neighbour_two=two;
+	}
 	
 }
