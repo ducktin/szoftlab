9f0b9d6b9958a4939c145a7ee7a2c3474b178eb8
Martin Borbola <ducktin@users.noreply.github.com>
2017-03-19 23:45:05 +0100

    by Labancz

--- src/szoftlab/TunnelEntrace.java
+++ src/szoftlab/TunnelEntrace.java
@@ -1,36 +1,33 @@
 package szoftlab;
 
+import java.util.Scanner;
+
 public class TunnelEntrace {
 
-	private boolean active=false;
 	private String Name;
 	
-	public TunnelEntrace(boolean act)
-		{active=act;}
-	
-	public String toString()
+	public String toString() //kiírjuk a nevét a bejáratnak
 		{
-			return "Name: " + Name + " Activities:" + active; 
-		}
-	
-	public void activate()
-		{
-			active=true;
-		}
-	
-	public void deactivate()
-		{
-			active=false;
-		}
-	
-	public boolean GetActive()
-		{
-			return active;
+			return "Name: " + Name; 
 		}
 
-	public String GetName()
-	{
-		return Name;
-	}
+	
+	public void activate() //aktíváljuk a bejératot
+		{
+			TunnelManager tm=new TunnelManager(); //ezáltal hivatkozunk a tunnel managerre
+			System.out.println("activate: Aktiv?");
+			Scanner s1=new Scanner(System.in);
+			if(s1.equals(true)) //aktív az adott bejárat?
+				{
+					tm.Substractactive(this); //meghívjuk a TM deaktiváló függvényét
+					s1.close();
+				}
+			else
+			{
+				s1.close();
+				tm.Addactive(this); //a TM aktiváló függvényét hívjuk
+			}
+			
+		}
 	
 }
