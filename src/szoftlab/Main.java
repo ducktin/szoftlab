8d450d7a43289a26e5a67e8a16bcd14c01172bb4
Máté <Mt@Blazestation-III>
2017-03-20 00:01:27 +0100

    komment

--- src/szoftlab/Main.java
+++ src/szoftlab/Main.java
@@ -42,14 +42,14 @@ public class Main {
 		}
 		
 
-		Rail r1 = new Rail("r1");
+		/*Rail r1 = new Rail("r1");		//Ez egy proba a mukodes Zoli
 		Rail r2 = new Rail("r2");
 		Locomotive l1 = new Locomotive(r1, null, "l1");
 		map.rails.add(r1);
 		map.rails.add(r2);
 		map.locomotives.add(l1);
 		SkeletonMethods.TrainMover(map);
-
+		*/
 		
 	}
 
