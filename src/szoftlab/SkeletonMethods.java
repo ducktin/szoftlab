4252b0915e8b8761f5b3c9720826f795664aaca8
Máté <Mt@Blazestation-III>
2017-03-19 21:20:54 +0100

    Oké ez már majd jó lesz ha lesz map

--- src/szoftlab/SkeletonMethods.java
+++ src/szoftlab/SkeletonMethods.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class SkeletonMethods {
 		
-	public void TrainMover(){
+	public static void TrainMover(){
 		System.out.println("Ki mozogjon? Kerem a nevet:");
-		Scanner s = new Scanner(System.in);
-		String tomove = s.nextLine(); // Itt kell megnézni, hogy van-e ilyen vonatunk.
-		s.close();
-		
-		
+		Scanner s1 = new Scanner(System.in);
+		String tomove = s1.nextLine(); // Itt kell megnézni, hogy van-e ilyen vonatunk.
 		System.out.println(tomove);
+		
+		System.out.println("Hova mozogjon? Kerem a nevet:");
+		String tohere = s1.nextLine(); // Itt kell megnézni, hogy van-e ilyen hely ahova mehetünk.
+		s1.close();
+		System.out.println(tohere);
 		
 	}
 }
