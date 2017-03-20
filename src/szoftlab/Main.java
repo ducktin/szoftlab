43a64c67e1b055e5563bb6a729d9d32f864558a6
Zoli <Zoli@Zotya>
2017-03-20 11:14:30 +0100

    menu scannert beraktam

--- src/szoftlab/Main.java
+++ src/szoftlab/Main.java
@@ -9,15 +9,14 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		TunnelEntrace t1=new TunnelEntrace("Tunnel1");
-		t1.activate();
 		
 		
 		
-		/*Map map = new Map();
+		
+		Map map = new Map();
 		
 		String choice = null;
-        Scanner scan = new Scanner(System.in);
+        //Scanner scan = new Scanner(System.in);
 		
 		do{
 			
@@ -28,13 +27,12 @@ public class Main {
 					+"4. Alagutszaj aktivalasa\n"
 					+"5. Valto allitasa\n"
 					);
-			
+			Scanner scan = new Scanner(System.in);
 			//Scanner scan = new Scanner(System.in);
-			/*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			line = br.readLine();
-			if(line.equals("0")) break;
-			//br.close();*/
-			/*choice = scan.nextLine();
+			//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			//if(line.equals("0")) break;
+			//br.close();
+			choice = scan.nextLine();
 			
 			switch(choice){
 			case "1":
@@ -103,20 +101,59 @@ public class Main {
 		}while(true);
 		
 
-
-		/*Rail r1 = new Rail("r1");		//Ez egy proba a mukodes Zoli
-		Rail r2 = new Rail("r2");
-		Locomotive l1 = new Locomotive(r1, null, "l1");
-		map.rails.add(r1);
-		map.rails.add(r2);
-		map.locomotives.add(l1);
-
-		SkeletonMethods.TrainMover(map);*/
+        /*System.out.println("lefutas:\n1. Tesztpalya inicializasa\n"
+					+"2. Vonat mozgasa\n"
+					+"3. Kocsi allomasokhoz erkezese\n"
+					+"4. Alagutszaj aktivalasa\n"
+					+"5. Valto allitasa\n"
+					);
+        
+        SkeletonMethods.Init(map);
 		
-
-		//SkeletonMethods.TrainMover(map);
-
 		
+		
+        SkeletonMethods.TrainMover(map);
+        
+        
+        Rail r3 = new Rail("r3");
+		Rail r4 = new Rail("r4");
+		Rail r5 = new Rail("r5");
+		TrainCart tr1 = new TrainCart(r3,null,"tr1",Color.GREEN); // R3-on a tr1
+		Locomotive l2 = new Locomotive(r4, tr1, "l2"); // R4-en a loco
+		Station st1 = new Station("st1", Color.GREEN,r4); // R4 az allomas --> r3rol r4rer green green hivodik az emptCart
+		//Palya kialakitasa
+		map.stats.add(st1);
+		map.locomotives.add(l2);
+		map.traincarts.add(tr1);
+		map.rails.add(r3);
+		map.rails.add(r4);	
+		map.rails.add(r5);
+		SkeletonMethods.TrainMover(map);
+		//Stationra erkezes szimulacioja
+		
+		if(tr1.getIsFirst()){ //ha if nincs akkor no line found
+			
+			if((tr1.currentPlace == st1.r) && (tr1.cartColor == st1.getColor())){
+				tr1.empty();//kiurul a kocsi es az empty true lesz 
+				
+				
+			}
+			else
+				System.out.println("Nem egyezik a szinuk");
+		}
+		
+		
+		//Implementalasa kikommentezbe a TunnelEntrace es TunnelManager osztalyokban
+		TunnelEntrace te1 = new TunnelEntrace("te1");
+		TunnelEntrace te2 = new TunnelEntrace("te2");
+		TunnelEntrace te3 = new TunnelEntrace("te3");
+		te1.activate();
+		te2.activate();
+		te3.activate();
+		
+		 
+		
+		SkeletonMethods.Switchswitcher(map);*/
 	}
 
 
